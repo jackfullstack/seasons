fc82ba34c11865416d8481a1bf91d53cf0f69f33
jackfullstack <ingurz@icloud.com>
2021-03-05 16:27:42 +0000

    Seasons first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner.js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    // Use this to dataload any info that only gets done once after component initialisation
+    navigator.geolocation.getCurrentPosition(
+      (position) =>
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+    console.log("component mounted and is updating mofo!");
+  }
+
+  componentDidUpdate() {
+    console.log("The component updated");
+  }
+
+  renderContent() {
+    if (this.state.lat && this.state.long && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    if (!this.state.lat && !this.state.long && this.state.errorMessage) {
+      return <div>{this.state.errorMessage}</div>;
+    }
+    return (
+      <div>
+        <Spinner message="Please accept location request" />
+      </div>
+    );
+  }
+
+  //React says we have to define render!!
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
